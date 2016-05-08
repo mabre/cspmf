541ecff31979ca1f971dc1ad7cd87dfd2ccc18c0
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2016-05-08 19:49:20 +0200

    make.sh: only containing build order

---
+++ CSPM-Frontend/make.sh
@@ -0,0 +1,26 @@
+src/Language/CSPM/Map.fr #TODO move
+src/Language/CSPM/Set.fr
+src/Language/CSPM/FilePath.fr
+src/Language/CSPM/TokenClasses.hs
+src/Language/CSPM/Token.hs
+src/Language/CSPM/SrcLoc.hs
+src/Language/CSPM/AlexWrapper.hs
+src/Language/CSPM/LexerAlex.hs
+src/Language/CSPM/AST.hs
+src/Language/CSPM/UnicodeSymbols.hs
+src/Language/CSPM/LexHelper.hs
+
+src/Text/ParserCombinators/Parsec/Error.hs
+src/Text/ParserCombinators/Parsec/Pos.hs
+src/Text/ParserCombinators/Parsec/Prim.hs
+src/Text/ParserCombinators/Parsec/Char.hs
+src/Text/ParserCombinators/Parsec/Combinator.hs
+src/Text/ParserCombinators/Parsec/Expr.hs
+src/Text/ParserCombinators/Parsec/ExprM.hs
+src/Text/ParserCombinators/Parsec/Token.hs
+src/Text/ParserCombinators/Parsec/Language.hs
+
+src/Language/CSPM/Parser.hs
+src/Language/CSPM/BuiltIn.hs
+src/Language/CSPM/Rename.hs
+src/Language/CSPM/Utils.hs
