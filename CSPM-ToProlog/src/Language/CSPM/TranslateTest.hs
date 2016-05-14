e4053698e0b8128be5c6ca15eaf3e53a89913430
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2016-05-15 00:08:11 +0200

    whoooop translation successful
    (except for deadbeef and version numbers, will do thourough testing in 12 hours:)

--- CSPM-ToProlog/src/Language/CSPM/TranslateTest.hs
+++ CSPM-ToProlog/src/Language/CSPM/TranslateTest.hs
@@ -2,5 +2,7 @@ module TranslateTest where
 
 import Language.CSPM.TranslateToProlog
 
-main = translateExpToPrologTerm Nothing "1+2"
+-- main = translateExpToPrologTerm Nothing "1+2"
 -- '+'('int'(1),'int'(1)).
+
+main = translateToProlog "/home/markus/Downloads/frege/cspmf/CSPM-Frontend/test/cspm/peterson.csp" "/tmp/peterson.frege.pl"
