55d97c1e0b9ef54c9bda96270fd63c137da82faf
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2016-06-04 08:48:57 +0200

    +Benchmark.java

--- CSPM-cspm-frontend/make.sh
+++ CSPM-cspm-frontend/make.sh
@@ -9,5 +9,5 @@ mkdir $build_dir 2> /dev/null
 step "Compiling CSPM-cspm-frontend"
 $fregec -d $build_dir -make -sp src/Main ExecCommand.fr ExceptionHandler.fr
 success fregec $?
-$javac -d $build_dir -cp $build_dir:$fregejar:../Libraries/commons-cli-1.3.1.jar src/Main/Main.java
+$javac -d $build_dir -cp $build_dir:$fregejar:../Libraries/commons-cli-1.3.1.jar src/Main/Main.java src/Main/Benchmark.java
 success javac $?
