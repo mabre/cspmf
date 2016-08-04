992ac066456f12cab8a628a78e1c05399d0540f0
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2016-08-04 19:47:34 +0200

    Text.XML.Light.Output ported

--- Libraries/src/Text/XML/Light/Output.hs
+++ Libraries/src/Text/XML/Light/Output.hs
@@ -1,65 +1,55 @@
---------------------------------------------------------------------
--- |
--- Module    : Text.XML.Light.Output
--- Copyright : (c) Galois, Inc. 2007
--- License   : BSD3
---
--- Maintainer: Iavor S. Diatchki <diatchki@galois.com>
--- Stability : provisional
--- Portability:
---
--- Output handling for the lightweight XML lib.
---
+{--
+    Output handling for the lightweight XML lib.
+    
+    [Module]     Text.XML.Light.Output
+    [Copyright]  (c) Galois, Inc. 2007
+    [License]    BSD3
+    
+    [Maintainer] Iavor S. Diatchki <diatchki@galois.com>
+    [Stability]  provisional
+-}
 
-module Text.XML.Light.Output
-  ( showTopElement, showContent, showElement, showCData, showQName, showAttr
-  , ppTopElement, ppContent, ppElement
-  , ppcTopElement, ppcContent, ppcElement
-  , ConfigPP
-  , defaultConfigPP, prettyConfigPP
-  , useShortEmptyTags, useExtraWhiteSpace
-  , tagEnd, xml_header
-  ) where
+module Text.XML.Light.Output where
 
 import Text.XML.Light.Types
-import Data.Char
-import Data.List ( isPrefixOf )
+import Data.Char hiding (isNumber)
 
--- | The XML 1.0 header
+--- The XML 1.0 header
 xml_header :: String
 xml_header = "<?xml version='1.0' ?>"
 
 
---------------------------------------------------------------------------------
-data ConfigPP = ConfigPP
+-- ------------------------------------------------------------------------------
+abstract data ConfigPP = ConfigPP
   { shortEmptyTag :: QName -> Bool
   , prettify      :: Bool
   }
 
--- | Default pretty orinting configuration.
---  * Always use abbreviate empty tags.
+--- Default pretty printing configuration.
+---  * Always use abbreviate empty tags.
 defaultConfigPP :: ConfigPP
-defaultConfigPP = ConfigPP { shortEmptyTag = const True
+defaultConfigPP = ConfigPP.ConfigPP
+                           { shortEmptyTag = const True
                            , prettify      = False
                            }
 
--- | The predicate specifies for which empty tags we should use XML's
--- abbreviated notation <TAG />.  This is useful if we are working with
--- some XML-ish standards (such as certain versions of HTML) where some
--- empty tags should always be displayed in the <TAG></TAG> form.
+--- The predicate specifies for which empty tags we should use XML's
+--- abbreviated notation <TAG />.  This is useful if we are working with
+--- some XML-ish standards (such as certain versions of HTML) where some
+--- empty tags should always be displayed in the <TAG></TAG> form.
 useShortEmptyTags :: (QName -> Bool) -> ConfigPP -> ConfigPP
-useShortEmptyTags p c = c { shortEmptyTag = p }
+useShortEmptyTags p c = c.{ shortEmptyTag = p }
 
 
--- | Specify if we should use extra white-space to make document more readable.
--- WARNING: This adds additional white-space to text elements,
--- and so it may change the meaning of the document.
+--- Specify if we should use extra white-space to make document more readable.
+--- WARNING: This adds additional white-space to text elements,
+--- and so it may change the meaning of the document.
 useExtraWhiteSpace :: Bool -> ConfigPP -> ConfigPP
-useExtraWhiteSpace p c  = c { prettify = p }
+useExtraWhiteSpace p c  = c.{ prettify = p }
 
--- | A configuration that tries to make things pretty
--- (possibly at the cost of changing the semantics a bit
--- through adding white space.)
+--- A configuration that tries to make things pretty
+--- (possibly at the cost of changing the semantics a bit
+--- through adding white space.)
 prettyConfigPP     :: ConfigPP
 prettyConfigPP      = useExtraWhiteSpace True defaultConfigPP
 
@@ -67,33 +57,33 @@ prettyConfigPP      = useExtraWhiteSpace True defaultConfigPP
 --------------------------------------------------------------------------------
 
 
--- | Pretty printing renders XML documents faithfully,
--- with the exception that whitespace may be added\/removed
--- in non-verbatim character data.
+--- Pretty printing renders XML documents faithfully,
+--- with the exception that whitespace may be added\/removed
+--- in non-verbatim character data.
 ppTopElement       :: Element -> String
 ppTopElement        = ppcTopElement prettyConfigPP
 
--- | Pretty printing elements
+--- Pretty printing elements
 ppElement          :: Element -> String
 ppElement           = ppcElement prettyConfigPP
 
--- | Pretty printing content
+--- Pretty printing content
 ppContent          :: Content -> String
 ppContent           = ppcContent prettyConfigPP
 
 
 
--- | Pretty printing renders XML documents faithfully,
--- with the exception that whitespace may be added\/removed
--- in non-verbatim character data.
+--- Pretty printing renders XML documents faithfully,
+--- with the exception that whitespace may be added\/removed
+--- in non-verbatim character data.
 ppcTopElement      :: ConfigPP -> Element -> String
 ppcTopElement c e   = unlines [xml_header,ppcElement c e]
 
--- | Pretty printing elements
+--- Pretty printing elements
 ppcElement         :: ConfigPP -> Element -> String
 ppcElement c e      = ppElementS c "" e ""
 
--- | Pretty printing content
+--- Pretty printing content
 ppcContent         :: ConfigPP -> Content -> String
 ppcContent c x      = ppContentS c "" x ""
 
@@ -101,39 +91,39 @@ ppcContent c x      = ppContentS c "" x ""
 
 
 
--- | Pretty printing content using ShowS
-ppContentS         :: ConfigPP -> String -> Content -> ShowS
-ppContentS c i x xs = case x of
+--- Pretty printing content using ShowS
+private ppContentS :: ConfigPP -> String -> Content -> ShowS
+private ppContentS c i x xs = case x of
                         Elem e -> ppElementS c i e xs
                         Text t -> ppCDataS c i t xs
                         CRef r -> showCRefS r xs
 
-ppElementS         :: ConfigPP -> String -> Element -> ShowS
-ppElementS c i e xs = i ++ (tagStart (elName e) (elAttribs e) $
-  case elContent e of
-    [] | "?" `isPrefixOf` qName name -> " ?>" ++ xs
-       | shortEmptyTag c name  -> " />" ++ xs
+private ppElementS :: ConfigPP -> String -> Element -> ShowS
+private ppElementS c i e xs = i ++ (tagStart e.elName e.elAttribs $
+  case e.elContent of
+    [] | name.qName.startsWith "?" -> " ?>" ++ xs
+       | c.shortEmptyTag name      -> " />" ++ xs
     [Text t] -> ">" ++ ppCDataS c "" t (tagEnd name xs)
-    cs -> '>' : nl ++ foldr ppSub (i ++ tagEnd name xs) cs
+    cs -> ">" ++ nl ++ foldr ppSub (i ++ tagEnd name xs) cs
       where ppSub e1 = ppContentS c (sp ++ i) e1 . showString nl
-            (nl,sp)  = if prettify c then ("\n","  ") else ("","")
+            (nl,sp)  = if c.prettify then ("\n","  ") else ("","")
   )
-  where name = elName e
+  where name = e.elName
 
-ppCDataS           :: ConfigPP -> String -> CData -> ShowS
-ppCDataS c i t xs   = i ++ if cdVerbatim t /= CDataText || not (prettify c)
+private ppCDataS   :: ConfigPP -> String -> CData -> ShowS
+private ppCDataS c i t xs   = i ++ if t.cdVerbatim /= CDataText || not c.prettify
                              then showCDataS t xs
-                             else foldr cons xs (showCData t)
+                             else foldr cons xs $ unpacked (showCData t)
 
   where cons         :: Char -> String -> String
         cons '\n' ys  = "\n" ++ i ++ ys
-        cons y ys     = y : ys
+        cons y ys     = ctos y ++ ys
 
 
 
---------------------------------------------------------------------------------
+-- ------------------------------------------------------------------------------
 
--- | Adds the <?xml?> header.
+--- Adds the <?xml?> header.
 showTopElement     :: Element -> String
 showTopElement c    = xml_header ++ showElement c
 
@@ -147,26 +137,27 @@ showCData          :: CData -> String
 showCData c         = ppCDataS defaultConfigPP "" c ""
 
 -- Note: crefs should not contain '&', ';', etc.
-showCRefS          :: String -> ShowS
-showCRefS r xs      = '&' : r ++ ';' : xs
+private showCRefS  :: String -> ShowS
+private showCRefS r xs = "&" ++ r ++ ";" ++ xs
 
--- | Convert a text element to characters.
-showCDataS         :: CData -> ShowS
-showCDataS cd =
- case cdVerbatim cd of
-   CDataText     -> escStr (cdData cd)
-   CDataVerbatim -> showString "<![CDATA[" . escCData (cdData cd)
+--- Convert a text element to characters.
+private showCDataS :: CData -> ShowS
+private showCDataS cd =
+ case cd.cdVerbatim of
+   CDataText     -> escStr cd.cdData
+   CDataVerbatim -> showString "<![CDATA[" . escCData cd.cdData
                                            . showString "]]>"
-   CDataRaw      -> \ xs -> cdData cd ++ xs
+   CDataRaw      -> \ xs -> cd.cdData ++ xs
 
---------------------------------------------------------------------------------
-escCData           :: String -> ShowS
-escCData (']' : ']' : '>' : cs) = showString "]]]]><![CDATA[>" . escCData cs
-escCData (c : cs)               = showChar c . escCData cs
-escCData []                     = id
+-- ------------------------------------------------------------------------------
+private escCData   :: String -> ShowS
+private escCData cs | cs.startsWith "]]>"
+                    = showString "]]]]><![CDATA[>" . escCData (strtail cs 3)
+private escCData "" = id
+private escCData cs = showChar (head cs) . escCData (tail cs)
 
-escChar            :: Char -> ShowS
-escChar c = case c of
+private escChar    :: Char -> ShowS
+private escChar c = case c of
   '<'   -> showString "&lt;"
   '>'   -> showString "&gt;"
   '&'   -> showString "&amp;"
@@ -182,21 +173,21 @@ escChar c = case c of
     | otherwise -> showString "&#" . shows oc . showChar ';'
       where oc = ord c
 
-escStr             :: String -> ShowS
-escStr cs rs        = foldr escChar rs cs
+private escStr     :: String -> ShowS
+private escStr cs rs = foldr escChar rs (unpacked cs)
 
 tagEnd             :: QName -> ShowS
-tagEnd qn rs        = '<':'/':showQName qn ++ '>':rs
+tagEnd qn rs        = "</" ++ showQName qn ++ ">" ++ rs
 
-tagStart           :: QName -> [Attr] -> ShowS
-tagStart qn as rs   = '<':showQName qn ++ as_str ++ rs
- where as_str       = if null as then "" else ' ' : unwords (map showAttr as)
+private tagStart   :: QName -> [Attr] -> ShowS
+private tagStart qn as rs = "<" ++ showQName qn ++ as_str ++ rs
+  where as_str       = if null as then "" else " " ++ unwords (map showAttr as)
 
 showAttr           :: Attr -> String
-showAttr (Attr qn v) = showQName qn ++ '=' : '"' : escStr v "\""
+showAttr (Attr qn v) = showQName qn ++ "=\"" ++ escStr v "\""
 
 showQName          :: QName -> String
-showQName q         = pre ++ qName q
-  where pre = case qPrefix q of
+showQName q         = pre ++ q.qName
+  where pre = case q.qPrefix of
                 Nothing -> ""
                 Just p  -> p ++ ":"
